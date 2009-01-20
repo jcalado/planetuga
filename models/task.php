d9fc1a022006ae7d72b49afabd6df8aa29856da1
Joel Calado <joelcalado@gmail.com>
2009-01-20 19:19:15 +0000

    Alguns tweaks às tarefas de modo a funcionarem ;-)

--- models/task.php
+++ models/task.php
@@ -5,15 +5,15 @@ class Task extends AppModel {
 	var $validate = array(
 		'user_id' => array('numeric'),
 		'assigned_to' => array('numeric'),
-		'priority' => array('range'),
-		'subject' => array('alphanumeric'),
-		'task' => array('alphanumeric')
+		'priority' => array('numeric'),
+		'subject' => array('notEmpty'),
+		'task' => array('notEmpty')
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $belongsTo = array(
 			'User' => array('className' => 'User',
-								'foreignKey' => 'user_id', 'assigned_to',
+								'foreignKey' => 'user_id',
 								'conditions' => '',
 								'fields' => '',
 								'order' => ''
